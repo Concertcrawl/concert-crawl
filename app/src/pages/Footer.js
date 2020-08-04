5db68e9ba9de637402c37f9f94dcd8f5847b767b
Jordan Hicks <jhicks@jhicksdesign.com>
2020-08-04 17:11:51 -0600

    Updated accessibility warnings, fixed link issues with footer.

--- app/src/pages/Footer.js
+++ app/src/pages/Footer.js
@@ -5,8 +5,8 @@ export const Footer = () => {
   return (
     <>
       <Container fluid className="border-top border-dark d-flex justify-content-around bg-light">
-        <a href="#" className="text-primary lead">Contact Us</a>
-        <a href="#" className="text-primary lead">Privacy Policy</a>
+        <a href="/" className="text-primary lead">Contact Us</a>
+        <a href="/" className="text-primary lead">Privacy Policy</a>
       </Container>
     </>
   )
