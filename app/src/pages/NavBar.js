5aecec63d0af72125a2e1deee0b747839aea9ed5
Jordan Hicks <jhicks@jhicksdesign.com>
2020-08-04 14:52:26 -0600

    Created base layout for search functionality.

--- app/src/pages/NavBar.js
+++ app/src/pages/NavBar.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import { Nav, Navbar } from 'react-bootstrap';
 
-export const NavBar = () => (
-  <Navbar bg="dark" className="navbar-dark">
-    <Navbar.Brand href="/">Concert Crawl</Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="mr-auto">
-        <Nav.Item><Nav.Link href="/">Saved Concerts</Nav.Link></Nav.Item>
-        <Nav.Item><Nav.Link href="/about">Followed Bands</Nav.Link></Nav.Item>
-      </Nav>
-      <Nav className="ml-auto">
-        <Nav.Item><Nav.Link href="/about">Log In</Nav.Link></Nav.Item>
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>
-)
+export const NavBar = () => {
+  return (
+    <>
+      <Navbar bg="dark" className="navbar-dark">
+        <Navbar.Brand href="/">Concert Crawl</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Item><Nav.Link href="/">Saved Concerts</Nav.Link></Nav.Item>
+            <Nav.Item><Nav.Link href="/about">Followed Bands</Nav.Link></Nav.Item>
+          </Nav>
+          <Nav className="ml-auto">
+            <Nav.Item><Nav.Link href="/about">Log In</Nav.Link></Nav.Item>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </>
+  )
+}
