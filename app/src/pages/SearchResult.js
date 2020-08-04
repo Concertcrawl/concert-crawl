1c54cdc3a6b47cd54387432acd0a4bd8c142e4f5
Jordan Hicks <jhicks@jhicksdesign.com>
2020-08-04 17:11:51 -0600

    Updated accessibility warnings, fixed link issues with footer.

--- app/src/pages/SearchResult.js
+++ app/src/pages/SearchResult.js
@@ -9,7 +9,7 @@ export const SearchResult = () => {
           <Col md={1} className="d-none d-lg-flex">
           </Col>
           <Col md={2} className="d-none d-md-flex">
-            <img src="https://via.placeholder.com/200" className="float-right  img-fluid" alt="This is a placeholder image.">
+            <img src="https://via.placeholder.com/200" className="float-right  img-fluid" alt="This is a placeholder.">
             </img>
           </Col>
           <Col xs={12} sm={3} md={2} className="my-auto text-center">
@@ -20,11 +20,11 @@ export const SearchResult = () => {
             <h2 className="text-center">Concert Name <Button variant="outline-dark" className="border-0 p-0"><h2>&#65291;</h2></Button></h2>
           </Col>
           <Col xs={12} sm={3} md={4} lg={2} className="my-auto text-center">
-            <p className="lead">Headliner <Button variant="outline-dark" className="border-0 p-0"><h2>&#11088;</h2></Button></p>
+            <p className="lead">Headliner <Button variant="outline-dark" className="border-0 p-0"><h2><span role="img" aria-label="Star">&#11088;</span></h2></Button></p>
             <p className="lead">Concert Location</p>
           </Col>
           <Col md={2} className="d-none d-md-flex">
-            <img src="https://via.placeholder.com/200" alt="This is a placeholder image." className="img-fluid">
+            <img src="https://via.placeholder.com/200" alt="This is a placeholder." className="img-fluid">
             </img>
           </Col>
           <Col md={1} className = "d-none d-lg-flex">
