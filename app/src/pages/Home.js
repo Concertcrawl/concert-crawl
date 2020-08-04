9f17474bb7b58d3a2cc367140b795c78f6152dd0
Jordan Hicks <jhicks@jhicksdesign.com>
2020-08-04 16:58:03 -0600

    Updated header sizing, adjusted responsive positioning.

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -9,45 +9,49 @@ export const Home = () => {
         <Jumbotron fluid>
           <h1 className="text-center">Concert Crawl Placeholder</h1>
         </Jumbotron>
-        <h2 className="text-center">About ConcertCrawl</h2>
-        <p>
-          Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.
+        <Container fluid className="bg-light py-3">
+          <h2 className="text-center">About ConcertCrawl</h2>
+          <p>
+            Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.
 
-          Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring.
+            Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring.
 
-          Capitalize on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will close the loop on focusing solely on the bottom line.
-        </p>
-        <Container className="mb-3">
-          <Form>
-            <Form.Group>
-              <Form.Label>Concert Search</Form.Label>
-              <Form.Control type="text"/>
-            </Form.Group>
-            <Form.Group>
-              <Row>
-                <Col xs={6} md={3} className="ml-md-5">
-                  <Form.Label>Genre</Form.Label>
-                  <Form.Control type="text"/>
-                </Col>
-                <Col xs={6} md={3}>
-                  <Form.Label>Location</Form.Label>
-                  <Form.Control type="text"/>
-                </Col>
-                <Col xs={5} md={2} className="ml-3 ml-md-0">
-                  <Form.Label>Start Date</Form.Label>
-                  <Form.Control type="text"/>
-                </Col>
-                <h1 className="display-3">-</h1>
-                <Col xs={5} md={2}>
-                  <Form.Label>End Date</Form.Label>
-                  <Form.Control type="text"/>
-                </Col>
-              </Row>
-            </Form.Group>
-            <Button variant="primary" size="lg" block>
-              Search!
-            </Button>
-          </Form>
+            Capitalize on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will close the loop on focusing solely on the bottom line.
+          </p>
+        </Container>
+        <Container fluid className="bg-dark py-3">
+          <Container className="mb-3 text-light">
+            <Form>
+              <Form.Group>
+                <Form.Label className="mx-auto"><h2>Concert Search</h2></Form.Label>
+                <Form.Control type="text"/>
+              </Form.Group>
+              <Form.Group>
+                <Row>
+                  <Col xs={6} md={3} className="ml-md-5">
+                    <Form.Label>Genre</Form.Label>
+                    <Form.Control type="text"/>
+                  </Col>
+                  <Col xs={6} md={3}>
+                    <Form.Label>Location</Form.Label>
+                    <Form.Control type="text"/>
+                  </Col>
+                  <Col xs={5} md={2} className="ml-3 ml-md-0">
+                    <Form.Label>Start Date</Form.Label>
+                    <Form.Control type="text"/>
+                  </Col>
+                  <h1 className="display-3">-</h1>
+                  <Col xs={5} md={2}>
+                    <Form.Label>End Date</Form.Label>
+                    <Form.Control type="text"/>
+                  </Col>
+                </Row>
+              </Form.Group>
+              <Button variant="primary" size="lg" block>
+                Search!
+              </Button>
+            </Form>
+          </Container>
         </Container>
         <SearchResult/>
         <SearchResult/>
--- app/src/pages/SearchResult.js
+++ app/src/pages/SearchResult.js
@@ -4,7 +4,7 @@ import { Container, Row, Col, Button} from 'react-bootstrap'
 export const SearchResult = () => {
   return (
     <>
-      <Container fluid className="pb-3">
+      <Container fluid className="pb-3 mt-3">
         <Row className="border-top border-bottom border-dark">
           <Col md={1} className="d-none d-lg-flex">
           </Col>
@@ -12,14 +12,14 @@ export const SearchResult = () => {
             <img src="https://via.placeholder.com/200" className="float-right  img-fluid" alt="This is a placeholder image.">
             </img>
           </Col>
-          <Col xs={4} sm={2} md={2} className="my-auto text-center">
+          <Col xs={12} sm={3} md={2} className="my-auto text-center">
             <p className="lead">Date</p>
             <p className="lead">Start Time</p>
           </Col>
-          <Col xs={6} sm={4} md={2} className="my-auto">
+          <Col xs={12} sm={6} md={2} className="my-auto">
             <h2 className="text-center">Concert Name <Button variant="outline-dark" className="border-0 p-0"><h2>&#65291;</h2></Button></h2>
           </Col>
-          <Col xs={4} sm={2} md={4} lg={2} className="my-auto text-center">
+          <Col xs={12} sm={3} md={4} lg={2} className="my-auto text-center">
             <p className="lead">Headliner <Button variant="outline-dark" className="border-0 p-0"><h2>&#11088;</h2></Button></p>
             <p className="lead">Concert Location</p>
           </Col>
