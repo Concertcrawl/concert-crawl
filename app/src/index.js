408c60fc5ebca6380e980d7c2f00e75c24697bc1
Jordan Hicks <jhicks@jhicksdesign.com>
2020-08-04 15:05:48 -0600

    Merged ConcertSearch into main Home file.

--- app/src/index.js
+++ app/src/index.js
@@ -5,10 +5,12 @@ import {BrowserRouter} from "react-router-dom";
 import {Route, Switch} from "react-router";
 import {FourOhFour} from "./pages/FourOhFour";
 import {Home} from "./pages/Home";
+import { NavBar } from './pages/NavBar'
 
 const Routing = () => (
   <>
     <BrowserRouter>
+      <NavBar/>
       <Switch>
         <Route exact path="/" component={Home}/>
         <Route component={FourOhFour}/>
