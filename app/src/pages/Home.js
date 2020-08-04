32f0217db27009b3056a42406e3e67638cf95315
Jordan Hicks <jhicks@jhicksdesign.com>
2020-08-04 14:52:26 -0600

    Created base layout for search functionality.

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -1,11 +1,17 @@
 import React from "react"
+import { Jumbotron } from 'react-bootstrap'
 import { NavBar } from './NavBar'
+import { SearchFunctionality } from './ConcertSearch'
 
 export const Home = () => {
   return (
     <>
       <NavBar/>
-      <h1>Home</h1>
+      <Jumbotron fluid>
+        <h1>This is a test of the ConcertCrawl broadcasting system.</h1>
+        <p>Let us test you.</p>
+      </Jumbotron>
+      <SearchFunctionality/>
     </>
   )
 }
