89670d9226e832b691c51712722a684f709119a1
Jordan Hicks <jhicks@jhicksdesign.com>
2020-08-04 17:06:19 -0600

    Added footer component.

---
+++ app/src/pages/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+
+export const Footer = () => {
+  return (
+    <>
+      <Container fluid className="border-top border-dark d-flex justify-content-around bg-light">
+        <a href="#" className="text-primary lead">Contact Us</a>
+        <a href="#" className="text-primary lead">Privacy Policy</a>
+      </Container>
+    </>
+  )
+}
