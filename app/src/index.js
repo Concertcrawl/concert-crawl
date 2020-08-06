b84a720cfabec6dbc2327f5c0b270c20d9be4d34
Calvin Reed <calvintreed1@gmail.com>
2020-08-06 14:44:12 -0600

    Added Static view for Saved Concerts page.

--- app/src/index.js
+++ app/src/index.js
@@ -10,7 +10,7 @@ import { Footer } from './pages/Footer'
 import { PrivacyPage } from './pages/PrivacyPage'
 import { ContactUs } from './pages/ContactUs'
 import { RegisterUser } from './pages/RegisterUser'
-import { SavedResults } from './pages/SavedResults'
+import { SavedConcerts } from './pages/SavedConcerts'
 import { ConcertInfoModal} from './pages/ConcertInfoModal'
 
 const Routing = () => (
@@ -22,7 +22,7 @@ const Routing = () => (
         <Route path="/register" component={RegisterUser}/>
         <Route path="/privacy-page" component={PrivacyPage}/>
         <Route path="/contact-us" component={ContactUs}/>
-        <Route path="/SavedResults" component={SavedResults}/>
+        <Route path="/SavedConcerts" component={SavedConcerts}/>
         <Route path="/ConcertInfoModal" component={ConcertInfoModal}/>
         <Route component={FourOhFour}/>
       </Switch>
---
+++ app/src/pages/SavedConcerts.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {Container, Row} from 'react-bootstrap'
+import {SearchResult} from './SearchResult'
+
+export const SavedConcerts = () => {
+  return(
+  <>
+    <Container fluid className="border border-dark">
+      <Row>
+      <h1 className="text-center col-12">Saved Concerts</h1>
+      </Row>
+    </Container>
+
+    <SearchResult/>
+    <SearchResult/>
+    <SearchResult/>
+
+
+
+  </>
+  )
+}
