a749e57ffa1c4b1b6d1959f667195c862cc30f3e
Calvin Reed <calvintreed1@gmail.com>
2020-08-06 10:32:34 -0600

    Added Personal Settings Static View.

--- app/src/pages/PersonalSettings.js
+++ app/src/pages/PersonalSettings.js
@@ -4,7 +4,7 @@ import { Button, Col, Container, Form, Row} from 'react-bootstrap'
 export const PersonalSettings = () => {
   return(
     <>
-      <Container className="border border-dark p-0">
+      <Container className="border border-dark p-0 my-5">
         <Row>
           <Col xs={12} className="mt-5 mb-5">
           <h2 className="text-center pb-2">Welcome To Concert Crawl!</h2>
@@ -12,10 +12,27 @@ export const PersonalSettings = () => {
         </Row>
         <Container xs={12}>
           <Row>
-            <Col>
-
+            <Col className="border-right border-top border-dark py-3">
+              <Form>
+                <Form.Group controlId="formBasicFirstName">
+                  <Form.Label>First Name</Form.Label>
+                  <Form.Control type="text" placeholder="First Name" />
+                </Form.Group>
+                <Form.Group controlId="formBasicLastName">
+                  <Form.Label>Last Name</Form.Label>
+                  <Form.Control type="text" placeholder="Last Name" />
+                </Form.Group>
+                <Form.Group controlId="formBasicProfileName">
+                  <Form.Label>Profile Name</Form.Label>
+                  <Form.Control type="text" placeholder="Profile Name" />
+                </Form.Group>
+                <Form.Group controlId="formBasicLocation">
+                  <Form.Label>Location</Form.Label>
+                  <Form.Control type="text" placeholder="Location" />
+                </Form.Group>
+              </Form>
             </Col>
-            <Col className="border border-dark">
+            <Col className="py-3 border-dark border-top">
               <Form>
                 <Form.Group controlId="formBasicEmail">
                   <Form.Label>Email address</Form.Label>
