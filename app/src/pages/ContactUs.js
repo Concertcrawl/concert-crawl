1edbffc84601f8adf82a62355d437018668ba292
Dixie Cooper <dcooper30@cnm.edu>
2020-08-06 13:35:04 -0600

    Added Contact Us page, about me part

--- app/src/pages/ContactUs.js
+++ app/src/pages/ContactUs.js
@@ -1,19 +1,34 @@
 import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col, Image } from 'react-bootstrap'
 
 export const ContactUs = () => {
   return (
     <>
       <Container>
         <Row className= "border border-dark mt-5">
-          <Col xs = {12} >
-            <h1 className= "text-center"> About the Concert Crawl Team </h1>
-          </Col>
-          <Col>
-            <Row className= "border border-dark">
-              <img src = "https://via.placeholder.com/150" alt = "placeholder"/>
+          <Row className = "text-center">
+            <h1 className= "text-center border-bottom border-dark"> About the Concert Crawl Team </h1>
+          </Row>
+          <Row className= "d-flex border-bottom border-dark p-5">
+            <Col xs = {2}>
+              <Image src = "https://via.placeholder.com/150" roundedCircle alt = "placeholder"/>
+            </Col>
+              <Col xs ={10}>
+                <p> Calvin Reed: </p>
+              </Col>
             </Row>
-          </Col>
+            <Row className= "border-bottom border-dark p-5">
+              <Col xs = {2}>
+                <Image src = "https://via.placeholder.com/150" roundedCircle alt = "placeholder"/>
+              </Col>
+              <Col xs ={10}>
+                <p>Dixie Cooper: </p>
+              </Col>
+            </Row>
+            <Row className= "border-bottom border-dark p-5">
+              <Image src = "https://via.placeholder.com/150" roundedCircle alt = "placeholder"/>
+              <p> Jordan Hicks: </p>
+            </Row>
         </Row>
       </Container>
     </>
