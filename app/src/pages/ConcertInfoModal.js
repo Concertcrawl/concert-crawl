4cc084bacbd93e1cb3b35dea1aa6b0c1845fb73a
Jordan Hicks <jhicks@jhicksdesign.com>
2020-08-06 16:22:20 -0600

    Modal tinkering, edited width of modal.

--- app/src/pages/ConcertInfoModal.js
+++ app/src/pages/ConcertInfoModal.js
@@ -1,29 +1,35 @@
 import React from 'react'
-import {Container, Row, Col, Modal,} from 'react-bootstrap'
+import {Container, Row, Col, Modal} from 'react-bootstrap'
+import "../styles.css";
 
-export const ConcertInfoModal = () => {
+export const ConcertInfoModal = (props) => {
   return(
   <>
-    <Modal.Dialog className="border border-dark">
-      <Modal.Header closeButton>
-        <Modal.Title> Date - Headliner - Location @ Time</Modal.Title>
-      </Modal.Header>
+    <Modal
+      show={true}
+      dialogClassName="custom-modal-style"
+    >
+      <Modal.Dialog className="border border-dark">
+        <Modal.Header closeButton>
+          <Modal.Title> Date - Headliner - Location @ Time</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Container>
+            <Row>
+              <Col>
+              <img src="https://via.placeholder.com/120x240?text=Vertical+Banner" alt="Placeholder" className="col-12"></img>
+              </Col>
+              <Col >
+                <h1> Headliner @ venue</h1>
+                <h2> Date & Time</h2>
 
-      <Modal.Body>
-        <Container>
-          <Row>
-            <Col>
-            <img src="https://via.placeholder.com/120x240?text=Vertical+Banner" alt="Placeholder" className="col-12"></img>
-            </Col>
-            <Col >
-              <h1> Headliner @ venue</h1>
-              <h2> Date & Time</h2>
+              </Col>
+            </Row>
+          </Container>
+        </Modal.Body>
+      </Modal.Dialog>
+    </Modal>
 
-            </Col>
-          </Row>
-        </Container>
-      </Modal.Body>
-    </Modal.Dialog>
 
 
   </>
