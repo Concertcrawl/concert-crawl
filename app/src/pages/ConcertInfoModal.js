40c2a4e02c3f4f783d4cefb064e96f5bb7714881
Jordan Hicks <jhicks@jhicksdesign.com>
2020-08-09 18:56:44 -0600

    Updated modal so that it opens when clicking plus button.

--- app/src/pages/ConcertInfoModal.js
+++ app/src/pages/ConcertInfoModal.js
@@ -1,69 +1,94 @@
-import React, { useState } from 'react'
-import {Container, Row, Col, Modal, Button, Collapse} from 'react-bootstrap'
-import "../styles.css";
+import React, { forwardRef, useImperativeHandle, useState } from 'react'
+import { Button, Modal, Container, Row, Col, Collapse } from 'react-bootstrap'
 
-export const ConcertInfoModal = (props) => {
+export const ConcertInfoModal = forwardRef((props, ref) => {
   const [open, setOpen] = useState(false);
+  const [show, setShow] = React.useState(false);
 
-  return(
-  <>
-    <Modal
-      show={true}
-      dialogClassName="custom-modal-style"
-    >
-      <Modal.Dialog className="custom-modal-style">
-        <Modal.Header>
-          <h3 className='col-12 modal-title text-center'>Date - Headliner - Location @ Time
-            <Button type='button' className='close my-auto' data-dismiss='modal' aria-label='Close'>
-              <span aria-hidden='true'>&times;</span>
-            </Button>
-          </h3>
-        </Modal.Header>
-        <Modal.Body className="p-0">
-          <Container>
-            <Row>
-              <Col xs={12} md={4} className="d-flex justify-content-center">
-                <img src="https://via.placeholder.com/240x240?text=Vertical+Banner" alt="Placeholder" className="img-fluid my-3">
+  const handleShow = () => setShow(true);
+  const handleClose = () => setShow(false);
 
-              </img>
-              </Col>
-              <Col xs={12} md={8}>
-                <Container className="border mb-3">
-                  <h1> Headliner @ venue</h1>
-                </Container>
-                <Container className="border  my-3">
-                  <Row>
-                    <Col className="my-auto">
-                      <h2> Date & Time</h2>
-                    </Col>
-                    <Col>
-                      <Button className="my-3 btn-block btn-dark py-2">Purchase Tickets on Ticketmaster.</Button>
-                      <Button className="my-3 btn-block btn-dark py-2">Add this concert to your events!</Button>
-                    </Col>
-                  </Row>
-                </Container>
+  useImperativeHandle(ref, () => {
+    return {
+      openModal: () => handleShow()
+    }
+  });
 
-              </Col>
-            </Row>
-          </Container>
-          <Button variant="outline-dark"
-                  className="border-top border-bottom-0 border-right-0 border-left-0 btn-block p-0"
-                  onClick={() => setOpen(!open)}
-                  aria-controls="example-collapse-text"
-                  aria-expanded={open}
-          >v</Button>
-          <Collapse in={open}>
-            <div id="example-collapse-text">
-              <p className="display-4 border-bottom py-3 text-center">I really wanna<span role="img" aria-label="Star">&#11088;</span></p>
-              <p className="display-4 border-bottom py-3 text-center">Really really wanna<span role="img" aria-label="Star">&#11088;</span></p>
-              <p className="display-4 py-3 text-center">Jump off a bridge<span role="img" aria-label="Star">&#11088;</span></p>
-            </div>
-          </Collapse>
-        </Modal.Body>
-      </Modal.Dialog>
-    </Modal>
+  if (show) {
+    return (
+      <>
+        <Modal
+          show={show}
+          dialogClassName="custom-modal-style"
+          onHide={() => {
+            handleClose();
+            setOpen(false)
+          }}
+        >
+          <Modal.Dialog className="custom-modal-style">
+            <Modal.Header>
+              <h3 className='col-12 modal-title text-center'>Date - Headliner - Location @ Time
+                <Button type='button' className='close my-auto' onClick={() => {
+                  handleClose();
+                  setOpen(false);
+                }}
+                        aria-label='Close'>
+                  <span aria-hidden='true'>&times;</span>
+                </Button>
+              </h3>
+            </Modal.Header>
+            <Modal.Body className="p-0">
+              <Container>
+                <Row>
+                  <Col xs={12} md={4} className="d-flex justify-content-center">
+                    <img src="https://via.placeholder.com/240x240?text=Vertical+Banner" alt="Placeholder"
+                         className="img-fluid my-3">
 
+                    </img>
+                  </Col>
+                  <Col xs={12} md={8}>
+                    <Container className="border mb-3">
+                      <h1> Headliner @ venue</h1>
+                    </Container>
+                    <Container className="border  my-3">
+                      <Row>
+                        <Col className="my-auto">
+                          <h2> Date & Time</h2>
+                        </Col>
+                        <Col>
+                          <Button className="my-3 btn-block btn-dark py-2">Purchase Tickets on Ticketmaster.</Button>
+                          <Button className="my-3 btn-block btn-dark py-2">Add this concert to your events!</Button>
+                        </Col>
+                      </Row>
+                    </Container>
 
-  </>
-  )
-}
+                  </Col>
+                </Row>
+              </Container>
+              <Button variant="outline-dark"
+                      className="border-top border-bottom-0 border-right-0 border-left-0 btn-block p-0"
+                      onClick={() => setOpen(!open)}
+                      aria-controls="example-collapse-text"
+                      aria-expanded={open}
+              >v</Button>
+              <Collapse in={open}>
+                <div id="example-collapse-text">
+                  <p className="display-4 border-bottom py-3 text-center">I really wanna<span role="img"
+                                                                                              aria-label="Star">&#11088;</span>
+                  </p>
+                  <p className="display-4 border-bottom py-3 text-center">Really really wanna<span role="img"
+                                                                                                   aria-label="Star">&#11088;</span>
+                  </p>
+                  <p className="display-4 py-3 text-center">Jump off a bridge<span role="img"
+                                                                                   aria-label="Star">&#11088;</span></p>
+                </div>
+              </Collapse>
+            </Modal.Body>
+          </Modal.Dialog>
+        </Modal>
+      </>
+    )
+  }
+
+  return null;
+})
--- app/src/pages/SearchResult.js
+++ app/src/pages/SearchResult.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import { Container, Row, Col, Button} from 'react-bootstrap'
+import { ConcertInfoModal } from './ConcertInfoModal'
 
 export const SearchResult = () => {
+  const modalRef = React.useRef();
+
+  const openModal = () => {
+    modalRef.current.openModal()
+  }
+
   return (
+
     <>
+      <ConcertInfoModal ref={modalRef}/>
       <Container fluid className="pb-3 mt-3">
         <Row className="border-top border-bottom border-dark">
           <Col md={1} className="d-none d-lg-flex">
@@ -17,7 +26,7 @@ export const SearchResult = () => {
             <p className="lead">Start Time</p>
           </Col>
           <Col xs={12} sm={6} md={2} className="my-auto">
-            <h2 className="text-center">Concert Name <Button variant="outline-dark" className="border-0 p-0"><h2>&#65291;</h2></Button></h2>
+            <h2 className="text-center">Concert Name <Button variant="outline-dark" className="border-0 p-0" onClick={openModal}><h2>&#65291;</h2></Button></h2>
           </Col>
           <Col xs={12} sm={3} md={4} lg={2} className="my-auto text-center">
             <p className="lead">Headliner <Button variant="outline-dark" className="border-0 p-0"><h2><span role="img" aria-label="Star">&#11088;</span></h2></Button></p>
