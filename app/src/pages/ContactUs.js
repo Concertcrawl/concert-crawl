566c17a1ece8d81b4e880d21bf508c4816b5a863
Dixie Cooper <dcooper30@cnm.edu>
2020-08-10 14:45:43 -0600

    Added the contact Form at the bottom of Contact Us Page

--- app/src/pages/ContactUs.js
+++ app/src/pages/ContactUs.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, Row, Col, Image } from 'react-bootstrap'
+import { Container, Row, Col, Image, Form, Button } from 'react-bootstrap'
 
 export const ContactUs = () => {
   return (
@@ -29,6 +29,39 @@ export const ContactUs = () => {
           </Col>
         </Row>
       </Container>
+
+
+      <Container xs={5}>
+        <Row>
+          <Col className="border border-dark mt-5 mb-5 p-5" md={{ span: 6, offset: 3 }}>
+            <Form>
+              <Form.Group controlId="contactForm">
+                <Form.Group>
+                  <Form.Label>Name</Form.Label>
+                  <Form.Control type="name" placeholder="Enter your name" />
+                </Form.Group>
+
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Text className="text-muted">
+                  We'll never share your email with anyone else.
+                </Form.Text>
+              </Form.Group>
+
+              <Form.Group>
+                <Form.Label>Message</Form.Label>
+                <Form.Control as="textarea" rows="3" />
+              </Form.Group>
+
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
     </>
   )
 }
+
+
