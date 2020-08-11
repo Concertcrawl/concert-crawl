6b79184b8c9e817ce05e8672ba61201a3dcbd27c
Dixie Cooper <dcooper30@cnm.edu>
2020-08-11 08:12:50 -0600

    Fixed Privacy Page Padding

--- app/src/pages/PrivacyPage.js
+++ app/src/pages/PrivacyPage.js
@@ -4,17 +4,18 @@ import { Container, Row, Col } from 'react-bootstrap'
 export const PrivacyPage = () => {
   return (
     <>
+
       <Container className="border border-dark  mb-5 mt-5">
         <Row>
           <Col xs={12} className="p-0">
             <h1 fluid className="text-center mt-3 border-bottom border-dark"> Privacy Page </h1>
           </Col>
-          </Row>
-        <Row className ="mt-5">
-            <Col className ="ml-3">
-              <img src= "https://via.placeholder.com/200" alt="placeholder"/>
-            </Col>
-            <Col xs={9} className = "pb-5">
+        </Row>
+        <Row className="mt-5">
+          <Col className="ml-3">
+            <img src="https://via.placeholder.com/200" alt="placeholder"/>
+          </Col>
+          <Col xs={9} className="pb-5">
             <p>
               Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean
               collard greens dandelion okra wakame tomato. Dandelion cucumber earthnut pea peanut soko zucchini.
