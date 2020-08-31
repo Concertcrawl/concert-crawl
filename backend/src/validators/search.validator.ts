b4145fb7fdb70c28cb3a92a92f00d7dea19b78e7
Jordan Hicks <jhicks@jhicksdesign.com>
2020-08-31 08:26:21 -0600

    Forgot to add search validator.

---
+++ backend/src/validators/search.validator.ts
@@ -0,0 +1,49 @@
+export const searchValidator = {
+    name: {
+        escape: true,
+        trim: true,
+        optional: true,
+        isLength: {
+            errorMessage: 'Max input length is 100 characters.',
+            options: { max: 100 }
+        }
+    },
+    genre: {
+        trim: true,
+        escape: true,
+        optional: true,
+        isLength: {
+            errorMessage: 'Max input length is 100 characters.',
+            options: { max: 100 }
+        }
+    },
+   location: {
+        trim: true,
+        escape: true,
+        isNumeric: true,
+       optional: true,
+        isLength: {
+            errorMessage: 'Zip code must be 5 characters.',
+            options: { min: 5, max: 5 }
+        }
+    },
+    sDate: {
+        isDate: true,
+        trim: true,
+        optional: true,
+    },
+    eDate: {
+        isDate: true,
+        trim: true,
+        optional: true,
+    },
+    venue: {
+        escape: true,
+        trim: true,
+        optional: true,
+        isLength: {
+            errorMessage: 'Venue max name is 200.',
+            options: { max: 200 }
+        }
+    }
+};
