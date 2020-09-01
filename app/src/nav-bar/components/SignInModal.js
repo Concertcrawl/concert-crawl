906a02663fbcc697e2f256ef642e0ecac8f42249
Dixie Cooper <dcooper30@cnm.edu>
2020-09-01 16:16:56 -0600

    Added Nav Bar folder and beginning of log in functionality tying together backend and frontend

---
+++ app/src/nav-bar/components/SignInModal.js
@@ -0,0 +1,37 @@
+import React, {useState} from "react";
+import Button from 'react-bootstrap/Button'
+import Modal from 'react-bootstrap/Modal'
+
+
+export const signInModal = () => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        Sign In
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Sign In</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <SignInForm/>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+
+}
