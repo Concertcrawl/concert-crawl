c850fe4a82fe1647f4b85336dedc754238729b6b
Dixie Cooper <dcooper30@cnm.edu>
2020-09-02 10:14:01 -0600

    Started working on Formix

--- app/src/nav-bar/components/SignInForm.js
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-import {httpConfig} from '../../utils/http-config';
-
---
+++ app/src/shared/components/nav-bar/sign-in/SignInFormContent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+export const SignInFormContent = (props) => {
+  const {
+    status,
+    values,
+    errors,
+    touched,
+    dirty,
+    isSubmitting,
+
+  }
+}
