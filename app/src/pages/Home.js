4ca4d7a7a5fe823298a6e1fd459e18debed4dec2
Calvin Reed <calvintreed1@gmail.com>
2020-09-03 09:55:15 -0600

    Added some information on Home Page for Concert Crawl.

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -65,17 +65,14 @@ export const Home = () => {
         <Container fluid className="bg-light py-3">
           <h2 className="text-center">About ConcertCrawl</h2>
           <p>
-            Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to
-            corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow
-            the holistic world view of disruptive innovation via workplace diversity and empowerment.
+            Concert Crawl was birthed during the covid era, in an era where almost no live music was being shown. We
+            wanted to provide a user friendly experience to allow people to follow their favorite bands and see local
+            live concerts in their area.
 
-            Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going
-            forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined
-            cloud solution. User generated content in real-time will have multiple touchpoints for offshoring.
+            Few experiences match up to the feeling of seeing your favorite band live in concert, the joy of hearing
+            your favorite song live. We wanted to provide a user-first way to make sure that you can track and get
+            tickets to local concerts, in an easy and concise manner.
 
-            Capitalize on low hanging fruit to identify a ballpark value added activity to beta test. Override the
-            digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information
-            highway will close the loop on focusing solely on the bottom line.
           </p>
         </Container>
         <Container fluid className="bg-dark py-3">
