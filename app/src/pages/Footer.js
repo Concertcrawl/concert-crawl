857732af90bd3e44437a7db5c6dbaa3235deab64
Dixie Cooper <dcooper30@cnm.edu>
2020-09-04 08:46:03 -0600

    Added styling to the footer

--- app/src/pages/Footer.js
+++ app/src/pages/Footer.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 export const Footer = () => {
   return (
     <>
-      <Container fluid className="border-top border-dark d-flex justify-content-around bg-light">
+      <Container fluid className="footer border-top border-dark d-flex justify-content-around bg-light">
         <Link to="/contact-us" className="text-primary lead">Contact Us</Link>
         <Link to="/privacy-page">Privacy Policy</Link>
       </Container>
