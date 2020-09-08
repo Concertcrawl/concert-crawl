53ae513e4f164e222b9d82e98f439e17cdf8d96f
Dixie Cooper <dcooper30@cnm.edu>
2020-09-08 16:13:42 -0600

    Fixed styling on contact us page

--- app/src/pages/ContactUs.js
+++ app/src/pages/ContactUs.js
@@ -10,10 +10,9 @@ export const ContactUs = () => {
             <Col xs={12} className="px-0">
               <h1 className="text-center contact-us-header"> About the Concert Crawl Team </h1>
             </Col>
-            <Col xs={4} className="py-2 pt-3 ">
-              <Image src="calvin.JPG" roundedCircle alt="pic-contact-us"/>
-            </Col>
-            <Col xs={8} className="py-2 pt-3">
+
+            <Col xs={12}>
+              <Image className="float-left" src="calvin.JPG" roundedCircle alt="pic-contact-us"/>
               <p> Calvin Reed: Calvin is a New Mexican born and raised Web Developer, who has a passion for live music.
                 Spending much of his formative years in small venues and at house shows, he has a huge passion for live
                 music, especially punk and metal. </p>
@@ -24,28 +23,21 @@ export const ContactUs = () => {
                 <i className="fab fa-linkedin fa-2x pr-3"></i>
               </a>
             </Col>
-            <Col xs={4} className="py-2 pt-3">
-              <Image src="dixie.JPG" roundedCircle alt="pic-contact-us"/>
-            </Col>
-            <Col xs={8} className="py-2 pt-3">
+            <Col xs={12}>
+              <Image className="float-left" src="dixie.JPG" roundedCircle alt="pic-contact-us"/>
               <p>Dixie Cooper: Dixie is a Web Developer and educator who grew up in Albuquerque, New Mexico. Formally a
                 middle school coding teacher, she is now pursuing a career in Web Development. Dixie loves going to live
                 shows and her favorite band is Haim, who she has seen a total of 8 times in concert! </p>
               <a href="https://github.com/dixiecooper100">
                 <i className="fab fa-github-square fa-2x pr-3"></i>
               </a>
-
-                <a href="https://www.linkedin.com/in/dixie-cooper-19b650139/">
-                  <i className="fab fa-linkedin fa-2x pr-3"></i>
-                </a>
-
+              <a href="https://www.linkedin.com/in/dixie-cooper-19b650139/">
+                <i className="fab fa-linkedin fa-2x"></i>
+              </a>
             </Col>
-            <Col xs={4} className="py-2 pt-3">
-              <Image src="jordan.png" roundedCircle alt="pic-contact-us"/>
-            </Col>
-            <Col xs={8} className="py-2 pt-3">
+            <Col xs={12}>
+              <Image className="float-left" src="jordan.png" roundedCircle alt="pic-contact-us"/>
               <p> Jordan Hicks: </p>
-
               <a href="https://github.com/jhicksdesign">
                 <i className="fab fa-github-square fa-2x pr-3"></i>
                 <a href="https://www.linkedin.com/in/jordan-hicks-design/">
