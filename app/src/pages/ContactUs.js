dcdf512f6799af98ddd83da6b236cd8cdaf06f1a
Calvin Reed <calvintreed1@gmail.com>
2020-09-08 15:31:37 -0600

    Fixed some logo issues on the contact us page.

--- app/src/pages/ContactUs.js
+++ app/src/pages/ContactUs.js
@@ -10,7 +10,7 @@ export const ContactUs = () => {
             <Col xs={12} className="px-0">
               <h1 className="text-center contact-us-header"> About the Concert Crawl Team </h1>
             </Col>
-            <Col xs={4} className="py-2 pt-3">
+            <Col xs={4} className="py-2 pt-3 ">
               <Image src="calvin.JPG" roundedCircle alt="pic-contact-us"/>
             </Col>
             <Col xs={8} className="py-2 pt-3">
@@ -18,10 +18,10 @@ export const ContactUs = () => {
                 Spending much of his formative years in small venues and at house shows, he has a huge passion for live
                 music, especially punk and metal. </p>
               <a href="https://github.com/Calvintreed">
-                <i className="fab fa-github-square fa-2x"></i>
+                <i className="fab fa-github-square fa-2x pr-3"></i>
               </a>
               <a href="https://www.linkedin.com/in/calvin-reed-64b6391a9/">
-                <i className="fab fa-linkedin fa-2x"></i>
+                <i className="fab fa-linkedin fa-2x pr-3"></i>
               </a>
             </Col>
             <Col xs={4} className="py-2 pt-3">
@@ -32,11 +32,11 @@ export const ContactUs = () => {
                 middle school coding teacher, she is now pursuing a career in Web Development. Dixie loves going to live
                 shows and her favorite band is Haim, who she has seen a total of 8 times in concert! </p>
               <a href="https://github.com/dixiecooper100">
-                <i className="fab fa-github-square fa-2x"></i>
+                <i className="fab fa-github-square fa-2x pr-3"></i>
               </a>
 
                 <a href="https://www.linkedin.com/in/dixie-cooper-19b650139/">
-                  <i className="fab fa-linkedin fa-2x"></i>
+                  <i className="fab fa-linkedin fa-2x pr-3"></i>
                 </a>
 
             </Col>
@@ -47,9 +47,9 @@ export const ContactUs = () => {
               <p> Jordan Hicks: </p>
 
               <a href="https://github.com/jhicksdesign">
-                <i className="fab fa-github-square fa-2x"></i>
+                <i className="fab fa-github-square fa-2x pr-3"></i>
                 <a href="https://www.linkedin.com/in/jordan-hicks-design/">
-                  <i className="fab fa-linkedin fa-2x"></i>
+                  <i className="fab fa-linkedin fa-2x pr-3"></i>
                 </a>
               </a>
             </Col>
