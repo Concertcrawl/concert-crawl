237c413c112162c71e0f2b3094f6147f72659213
Dixie Cooper <dcooper30@cnm.edu>
2020-09-09 09:38:21 -0600

    Link to ticketmaster in footer and credit to ticketmaster for API/data

--- app/src/pages/Footer.js
+++ app/src/pages/Footer.js
@@ -7,8 +7,9 @@ export const Footer = () => {
     <>
 
       <Container fluid className="footer fixed-bottom d-flex justify-content-around bg-light">
-        <Link to="/contact-us" className="text-dark lead">Contact Us</Link>
-        <Link to="/privacy-page" className="text-dark lead">Privacy Policy</Link>
+        <Link to="/contact-us" className="text-dark">Contact Us</Link>
+        <a className="text-white" href="https://www.ticketmaster.com/">Data from Ticketmaster</a>
+        <Link to="/privacy-page" className="text-dark">Privacy Policy</Link>
       </Container>
     </>
   )
